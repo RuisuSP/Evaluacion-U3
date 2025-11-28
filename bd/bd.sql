b912dded694a330ed1f7d4fb9428bc0c89600a01
Luis <silvapachecoangel@gmail.com>
2025-11-27 22:00:25 -0600

    1.0.0. Creación base de datos

---
+++ bd/bd.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS db_paquexpress;
+USE db_paquexpress;
+
+-- TABLAS
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
+    rol ENUM('admin', 'repartidor') DEFAULT 'repartidor'
+);
+
+CREATE TABLE paquetes (
+    id_paquete INT AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(200) NOT NULL,
+    direccion_destino VARCHAR(255) NOT NULL,
+    id_repartidor INT,
+    estado ENUM('pendiente', 'entregado') DEFAULT 'pendiente',
+    foto_evidencia LONGTEXT,
+    ubicacion VARCHAR(100),
+    fecha DATETIME,
+    
+    FOREIGN KEY (id_repartidor) REFERENCES usuarios(id_usuario)
+);
+
+
+-- DATOS DE PRUEBA
+-- Usuario de prueba con contraseña encriptada con md5 (Password:12345)
+INSERT INTO usuarios (nombre, correo, password, rol) 
+VALUES ('Pedro Perez', 'pedrop@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'repartidor');
+
+-- Paquetes de prueba asignados a Juan
+INSERT INTO paquetes (descripcion, direccion_destino, id_repartidor, estado) 
+VALUES 
+('Zapatos Nike Low Dunk Rojos', 'Av. 5 de Febrero #123, Qro', 1, 'pendiente'),
+('Laptop Dell Latitude', 'Blvd. Bernardo Quintana #400, Qro', 1, 'pendiente'),
+('Libro Larousse', 'Calle Madero #10, Centro', 1, 'pendiente');
